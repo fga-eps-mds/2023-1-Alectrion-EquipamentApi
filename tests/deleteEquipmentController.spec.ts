7524cff9cda59d8ea785364bc331f59a1330c3ca
Pedro Augusto Dourado Izarias <pedroaugustoizarias@gmail.com>
2023-05-10 01:08:05 -0300

    #76 feat: adicionando teste

---
+++ tests/deleteEquipmentController.spec.ts
@@ -0,0 +1,107 @@
+import { MockProxy, mock } from 'jest-mock-extended'
+
+import { NullFields } from '../src/useCases/createEquipment/createEquipmentUseCase'
+import { DeleteEquipmentController } from '../src/presentation/controller/deleteEquipmentController'
+
+import { HttpResponse } from '../src/presentation/helpers/http'
+import { ServerError, UnauthorizedError } from '../src/presentation/errors'
+import {
+  DeleteEquipmentUseCase,
+  DeleteEquipmentUseCaseData,
+  InvalidEquipmentError,
+  TimeLimitError
+} from '../src/useCases/deleteEquipment/deleteEquipmentUseCase'
+
+describe('Delete equipment controller', () => {
+  let deleteEquipmentUseCase: MockProxy<DeleteEquipmentUseCase>
+  let deleteEquipmentController: DeleteEquipmentController
+
+  beforeEach(() => {
+    deleteEquipmentUseCase = mock()
+    deleteEquipmentController = new DeleteEquipmentController(
+      deleteEquipmentUseCase
+    )
+  })
+
+  test('should get a successful response', async () => {
+    const data: DeleteEquipmentUseCaseData = {
+      id: '130265af-6afd-494d-b025-e657db264e56'
+    }
+
+    deleteEquipmentUseCase.execute.mockResolvedValue({
+      isSuccess: true
+    })
+
+    const response: HttpResponse = await deleteEquipmentController.perform(data)
+
+    expect(response).toHaveProperty('statusCode', 200)
+  })
+
+  test('should get a bad request response', async () => {
+    const data: DeleteEquipmentUseCaseData = {
+      id: '130265af-6afd-494d-b025-e657db264e56'
+    }
+
+    deleteEquipmentUseCase.execute.mockResolvedValue({
+      isSuccess: false,
+      error: new NullFields()
+    })
+
+    const response: HttpResponse = await deleteEquipmentController.perform(data)
+
+    expect(response).toHaveProperty('statusCode', 500)
+    expect(response).toHaveProperty('data')
+    expect(response.data).toBeInstanceOf(NullFields)
+  })
+
+  test('should get a invalid moviment error response', async () => {
+    const data: DeleteEquipmentUseCaseData = {
+      id: '130265af-6afd-494d-b025-e657db264e56'
+    }
+
+    deleteEquipmentUseCase.execute.mockResolvedValue({
+      isSuccess: false,
+      error: new InvalidEquipmentError()
+    })
+
+    const response: HttpResponse = await deleteEquipmentController.perform(data)
+
+    expect(response).toHaveProperty('statusCode', 404)
+    expect(response).toHaveProperty('data')
+    expect(response.data).toBeInstanceOf(InvalidEquipmentError)
+  })
+
+  test('should get a time limit error response', async () => {
+    const data: DeleteEquipmentUseCaseData = {
+      id: '130265af-6afd-494d-b025-e657db264e56'
+    }
+
+    deleteEquipmentUseCase.execute.mockResolvedValue({
+      isSuccess: false,
+      error: new TimeLimitError()
+    })
+
+    const response: HttpResponse = await deleteEquipmentController.perform(data)
+
+    expect(response).toHaveProperty('statusCode', 401)
+    expect(response).toHaveProperty('data')
+    expect(response.data).toBeInstanceOf(UnauthorizedError)
+  })
+
+  test('should return server error response', async () => {
+    const data: DeleteEquipmentUseCaseData = {
+      id: '130265af-6afd-494d-b025-e657db264e56'
+    }
+
+    deleteEquipmentUseCase.execute.mockResolvedValue({
+      isSuccess: false,
+      error: new ServerError()
+    })
+
+    const response: HttpResponse = await deleteEquipmentController.perform(data)
+
+    expect(response).toHaveProperty('statusCode', 500)
+    expect(response).toHaveProperty('data')
+    expect(response.data).toBeInstanceOf(ServerError)
+  })
+})
---
+++ tests/deleteEquipmentUseCase.spec.ts
@@ -0,0 +1,164 @@
+import { MockProxy, mock } from 'jest-mock-extended'
+
+import { Status } from '../src/domain/entities/equipamentEnum/status'
+import { Estado } from '../src/domain/entities/equipamentEnum/estado'
+import { Type } from '../src/domain/entities/equipamentEnum/type'
+
+import { Equipment as EquipmentDb } from '../src/db/entities/equipment'
+
+import { EquipmentRepositoryProtocol } from '../src/repository/protocol/equipmentRepositoryProtocol'
+
+import { CreateMovementUseCase,CreateMovementUseCaseData } from '../src/useCases/createMovement/createMovementUseCase'
+
+import {
+  DeleteEquipmentUseCase,
+  DeleteEquipmentUseCaseData,
+  InvalidEquipmentError,
+  TimeLimitError,
+  NullFieldsError,
+  EquipmentMovedError
+} from '../src/useCases/deleteEquipment/deleteEquipmentUseCase'
+import { MovementRepository } from '../src/repository/movementRepository'
+import { MovementRepositoryProtocol } from '../src/repository/protocol/movementRepositoryProtocol'
+
+describe('Delete equipments use case', () => {
+  let equipmentRepository: MockProxy<EquipmentRepositoryProtocol>
+  let movementRepository: MockProxy<MovementRepositoryProtocol>
+  let deleteEquipmentUseCase: DeleteEquipmentUseCase
+  let createMovementUseCase: CreateMovementUseCase
+
+  beforeEach(() => {
+    equipmentRepository = mock()
+
+    deleteEquipmentUseCase = new DeleteEquipmentUseCase(equipmentRepository,movementRepository)
+  })
+
+  test('should delete equipment', async () => {
+    const now = Date.now()
+    const mockedResult: EquipmentDb[] = [
+      {
+            id: 'c266c9d5-4e91-4c2e-9c38-fb8710d7e896',
+            tippingNumber: '123123',
+            serialNumber: '123',
+            type: Type.Nobreak,
+            situacao: Status.ACTIVE,
+            estado: Estado.Novo,
+            model: 'Xiaomi XT',
+            description: '',
+            initialUseDate: '2022-12-12',
+            acquisitionDate: new Date('2022-12-12'),
+            invoiceNumber: '123',
+            power: '220',
+            createdAt: new Date(now),
+            updatedAt: new Date(now)
+          }
+    ]
+
+    const data: DeleteEquipmentUseCaseData = {
+      id: 'c266c9d5-4e91-4c2e-9c38-fb8710d7e896'
+    }
+
+    equipmentRepository.genericFind
+      .mockResolvedValueOnce(mockedResult)
+      .mockResolvedValueOnce(mockedResult)
+    equipmentRepository.deleteOne.mockResolvedValueOnce(true)
+
+    const result = await deleteEquipmentUseCase.execute(data)
+
+    expect(result).toHaveProperty('isSuccess', true)
+  })
+
+  test('should not delete equipment with a movimentation associated', async () => {
+    const now = Date.now()
+    const mockedResult: EquipmentDb[] = [
+    {
+        id: 'c266c9d5-4e91-4c2e-9c38-fb8710d7e896',
+        tippingNumber: '123123',
+        serialNumber: '123',
+        type: Type.Nobreak,
+        situacao: Status.ACTIVE,
+        estado: Estado.Novo,
+        model: 'Xiaomi XT',
+        description: '',
+        initialUseDate: '2022-12-12',
+        acquisitionDate: new Date(now),
+        invoiceNumber: '123',
+        power: '220',
+        createdAt: new Date(now),
+        updatedAt: new Date(now)
+    }
+    ]
+
+    equipmentRepository.genericFind
+    .mockResolvedValueOnce(mockedResult)
+    .mockResolvedValueOnce(mockedResult)
+    equipmentRepository.deleteOne.mockResolvedValueOnce(true)
+
+    const data: DeleteEquipmentUseCaseData = {
+        id: 'c266c9d5-4e91-4c2e-9c38-fb8710d7e896'
+      }
+    
+    const move: CreateMovementUseCaseData = {
+        userid: '7f5a508d-b6d4-4011-9553-d181e75e1b09',
+        equipments: ['c266c9d5-4e91-4c2e-9c38-fb8710d7e896'],
+        type: 0,
+        destination: 'f2cf114d-51f4-4ccc-9c8f-64fd97e6cfb2',
+        inchargename: 'José Matheus',
+        inchargerole: 'Sargento',
+        chiefname: 'Matheus Texeira',
+        chiefrole: 'Delegado'
+      }
+    
+    const moveResult = await createMovementUseCase.execute(move)
+
+    const result = await deleteEquipmentUseCase.execute(data)
+
+    expect(moveResult).toHaveProperty('isSuccess', true)
+    expect(moveResult.data).toHaveProperty('equipments',['c266c9d5-4e91-4c2e-9c38-fb8710d7e896'])
+    expect(result).toHaveProperty('isSuccess', false)
+    expect(result).toHaveProperty('error')
+    expect(result.error).toBeInstanceOf(EquipmentMovedError)
+})
+
+  test('should not delete equipment after 10 minutes of creation', async () => {
+    const now = Date.now()
+    const tenMinutes =  60 * 10 * 1000
+
+    const mockedResult: EquipmentDb[] = [
+    {
+        id: 'c266c9d5-4e91-4c2e-9c38-fb8710d7e896',
+        tippingNumber: '123123',
+        serialNumber: '123',
+        type: Type.Nobreak,
+        situacao: Status.ACTIVE,
+        estado: Estado.Novo,
+        model: 'Xiaomi XT',
+        description: '',
+        initialUseDate: '2022-12-12',
+        acquisitionDate: new Date(now - tenMinutes),
+        invoiceNumber: '123',
+        power: '220',
+        createdAt: new Date(now - tenMinutes - 1),
+        updatedAt: new Date(now - tenMinutes - 1)
+      }
+    ]
+    
+    const data: DeleteEquipmentUseCaseData = {
+      id: 'c266c9d5-4e91-4c2e-9c38-fb8710d7e896'
+    }
+
+    equipmentRepository.genericFind
+      .mockResolvedValueOnce(mockedResult)
+      .mockResolvedValueOnce(mockedResult)
+    equipmentRepository.deleteOne.mockResolvedValueOnce(true)
+
+    const result = await deleteEquipmentUseCase.execute(data)
+
+    expect(result).toHaveProperty('isSuccess', false)
+    expect(result).toHaveProperty('error')
+    expect(result.error).toBeInstanceOf(TimeLimitError)
+  })
+
+})
+
+
