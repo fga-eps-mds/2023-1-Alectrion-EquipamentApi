bea64342282423f24446359f28be12e8e8e7fb7b
mibasFerraz <lucasllff@gmail.com>
2023-06-12 21:23:24 -0300

    Co-authored-by: JoaoVitorFarias <jvlopesfarias@gmail.com>

--- src/domain/entities/equipamentEnum/status.ts
+++ src/domain/entities/equipamentEnum/status.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 export enum Status {
   ACTIVE = 'Ativo',
-  ACTIVE_LOAN = 'Ativo Empréstimo',
+  ACTIVE_LOAN = 'Empréstimo',
   DOWNGRADED = 'Baixado',
   MAINTENANCE = 'Manutenção',
   TECHNICAL_RESERVE = 'Reserva Técnica',
