3228b822b1f0d3c08883992917adbf08c2e57498
JoaoVitorFarias <jvlopesfarias@gmail.com>
2023-07-06 22:37:00 -0300

    #164 feat: funcionalidade de busca de tipo

---
+++ src/presentation/controller/find-equipment-type.controller.ts
@@ -0,0 +1,28 @@
+import { EquipmentType } from '../../domain/entities/equipment-type'
+import { FindEquipmentTypeUseCase } from '../../useCases/find-equipment-type/find-equipment-type.use-case'
+import { HttpResponse, ok, serverError } from '../helpers'
+import { Controller } from '../protocols/controller'
+
+type Model = Error | EquipmentType[]
+
+type HttpRequest = {
+  search: string
+}
+
+export class FindEquimentTypeController extends Controller {
+  constructor(private readonly equipmentTypeUseCase: FindEquipmentTypeUseCase) {
+    super()
+  }
+
+  async perform(params: HttpRequest): Promise<HttpResponse<Model>> {
+    const response = await this.equipmentTypeUseCase.execute({
+      search: params.search
+    })
+
+    if (response.isSuccess && response.data) {
+      return ok(response.data)
+    } else {
+      return serverError(response.error)
+    }
+  }
+}
