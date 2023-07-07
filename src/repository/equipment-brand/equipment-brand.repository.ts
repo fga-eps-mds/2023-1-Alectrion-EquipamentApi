9420b874df7c798c42e2d266b97ee182c81bf48c
JoaoVitorFarias <jvlopesfarias@gmail.com>
2023-07-06 22:27:30 -0300

    #164 feat: adição de repositorio de marca

---
+++ src/repository/equipment-brand/equipment-brand.repository.ts
@@ -0,0 +1,9 @@
+import { EquipmentBrand } from '../../db/entities/equipment-brand'
+
+export interface EquipmentBrandRepository {
+  create(brand: EquipmentBrand): Promise<EquipmentBrand>
+  find(search: string): Promise<EquipmentBrand[]>
+  findByName(name: string): Promise<EquipmentBrand | undefined>
+  update(brand: EquipmentBrand): Promise<void>
+  delete(brandId: string): Promise<void>
+}
