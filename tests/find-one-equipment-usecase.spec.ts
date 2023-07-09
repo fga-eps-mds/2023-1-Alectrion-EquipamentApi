d50f0e9a1b0b43949d7f94ed22e6de129238e8f3
JoaoVitorFarias <jvlopesfarias@gmail.com>
2023-07-09 17:55:57 -0300

    #164 test: ajuste nos testes

--- tests/find-one-equipment-usecase.spec.ts
+++ tests/find-one-equipment-usecase.spec.ts
@@ -1,8 +1,10 @@
-import { EquipmentNotFoundError, FindOneEquipmentUseCase } from '../src/useCases/findOneEquipment/find-one-equipment-usecase';
+import {
+  EquipmentNotFoundError,
+  FindOneEquipmentUseCase
+} from '../src/useCases/findOneEquipment/find-one-equipment-usecase'
 import { MockProxy, mock } from 'jest-mock-extended'
 import { Estado } from '../src/domain/entities/equipamentEnum/estado'
 import { Status } from '../src/domain/entities/equipamentEnum/status'
-import { Type } from '../src/domain/entities/equipamentEnum/type'
 import { ListOneEquipmentRepository } from '../src/repository/equipment/list-one-equipment'
 
 describe('FindOneEquipment', () => {
@@ -12,41 +14,41 @@ describe('FindOneEquipment', () => {
   beforeEach(() => {
     listOneEquipmentRepository = mock()
     findOneEquipmentUseCase = new FindOneEquipmentUseCase(
-        listOneEquipmentRepository
-      )
+      listOneEquipmentRepository
+    )
   })
 
   test('should return equipment when found', async () => {
-    const expectedData = { 
-        id: 'id',
-        acquisitionDate: new Date(),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        situacao: Status.ACTIVE,
-        estado: Estado.Novo,
-        tippingNumber: '12345',
-        model: 'DELL G15',
-        serialNumber: 'any',
-        type: Type.CPU
-    };
-    listOneEquipmentRepository.findOne.mockResolvedValue(expectedData);
+    const expectedData = {
+      id: 'id',
+      acquisitionDate: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      situacao: Status.ACTIVE,
+      estado: Estado.Novo,
+      tippingNumber: '12345',
+      model: 'DELL G15',
+      serialNumber: 'any',
+      type: { id: 2, name: 'any', createdAt: new Date(), updatedAt: new Date() }
+    }
+    listOneEquipmentRepository.findOne.mockResolvedValue(expectedData)
 
-    const query = { tippingNumber: '12345', id: 'id' };
-    const result = await findOneEquipmentUseCase.execute(query);
+    const query = { tippingNumber: '12345', id: 'id' }
+    const result = await findOneEquipmentUseCase.execute(query)
 
-    expect(listOneEquipmentRepository.findOne).toHaveBeenCalledWith(query);
-    expect(result.isSuccess).toBe(true);
-    expect(result.data).toEqual(expectedData);
-  });
+    expect(listOneEquipmentRepository.findOne).toHaveBeenCalledWith(query)
+    expect(result.isSuccess).toBe(true)
+    expect(result.data).toEqual(expectedData)
+  })
 
   test('should return EquipmentNotFoundError when no equipment is found', async () => {
-    listOneEquipmentRepository.findOne.mockResolvedValue(null);
+    listOneEquipmentRepository.findOne.mockResolvedValue(null)
 
-    const query = { tippingNumber: '543' };
-    const result = await findOneEquipmentUseCase.execute(query);
+    const query = { tippingNumber: '543' }
+    const result = await findOneEquipmentUseCase.execute(query)
 
-    expect(listOneEquipmentRepository.findOne).toHaveBeenCalledWith(query);
-    expect(result.isSuccess).toBe(false);
-    expect(result.error).toBeInstanceOf(EquipmentNotFoundError);
-  });
-});
+    expect(listOneEquipmentRepository.findOne).toHaveBeenCalledWith(query)
+    expect(result.isSuccess).toBe(false)
+    expect(result.error).toBeInstanceOf(EquipmentNotFoundError)
+  })
+})
--- tests/find-order-service-usecase.spec.ts
+++ tests/find-order-service-usecase.spec.ts
@@ -1,10 +1,12 @@
-import { NotOSFoundError, FindOrderService } from '../src/useCases/find-order-service/find-order-service';
+import {
+  NotOSFoundError,
+  FindOrderService
+} from '../src/useCases/find-order-service/find-order-service'
 import { MockProxy, mock } from 'jest-mock-extended'
 import { Equipment } from '../src/domain/entities/equipment'
 import { Estado } from '../src/domain/entities/equipamentEnum/estado'
 import { Status } from '../src/domain/entities/equipamentEnum/status'
 import { Status as OSStatus } from '../src/domain/entities/serviceOrderEnum/status'
-import { Type } from '../src/domain/entities/equipamentEnum/type'
 import { OrderServiceRepositoryProtocol } from '../src/repository/protocol/orderServiceRepositoryProtocol'
 
 describe('FindOrderService', () => {
@@ -21,60 +23,72 @@ describe('FindOrderService', () => {
     tippingNumber: 'any',
     model: 'DELL G15',
     serialNumber: 'any',
-    type: Type.CPU
+    type: { id: 2, name: 'any', createdAt: new Date(), updatedAt: new Date() }
   }
 
   beforeEach(() => {
     orderServiceRepositoryProtocol = mock()
     findOrderServiceUseCase = new FindOrderService(
-        orderServiceRepositoryProtocol
-      )
+      orderServiceRepositoryProtocol
+    )
   })
 
   test('should return order services when they are found', async () => {
-    const expectedData = [{ 
-      id: 2,
-      equipment,
-      description: 'any_description',
-      seiProcess: '123456789',
-      senderPhone: '61992809831',
-      senderDocument: '12345678910',
-      technicianId: '123456',
-      technicianName: 'Pessoa',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      status: OSStatus.MAINTENANCE,
-      authorId: '123456789',
-      senderName: 'Pessoa 2'
-    }];
-    orderServiceRepositoryProtocol.findOrderServiceGeneric.mockResolvedValue(expectedData);
+    const expectedData = [
+      {
+        id: 2,
+        equipment,
+        description: 'any_description',
+        seiProcess: '123456789',
+        senderPhone: '61992809831',
+        senderDocument: '12345678910',
+        technicianId: '123456',
+        technicianName: 'Pessoa',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status: OSStatus.MAINTENANCE,
+        authorId: '123456789',
+        senderName: 'Pessoa 2'
+      }
+    ]
+    orderServiceRepositoryProtocol.findOrderServiceGeneric.mockResolvedValue(
+      expectedData
+    )
 
-    const query = { type: 'type1', unit: 'unit1' };
-    const result = await findOrderServiceUseCase.execute(query);
+    const query = { type: 'type1', unit: 'unit1' }
+    const result = await findOrderServiceUseCase.execute(query)
 
-    expect(orderServiceRepositoryProtocol.findOrderServiceGeneric).toHaveBeenCalledWith(query);
-    expect(result.isSuccess).toBe(true);
-    expect(result.data).toEqual(expectedData);
-  });
+    expect(
+      orderServiceRepositoryProtocol.findOrderServiceGeneric
+    ).toHaveBeenCalledWith(query)
+    expect(result.isSuccess).toBe(true)
+    expect(result.data).toEqual(expectedData)
+  })
 
   test('should return NotOSFoundError when no order services are found', async () => {
-    orderServiceRepositoryProtocol.findOrderServiceGeneric.mockResolvedValue(undefined);
+    orderServiceRepositoryProtocol.findOrderServiceGeneric.mockResolvedValue(
+      undefined
+    )
 
-    const query = { model: 'abc' };
-    const result = await findOrderServiceUseCase.execute(query);
+    const query = { model: 'abc' }
+    const result = await findOrderServiceUseCase.execute(query)
 
-    expect(orderServiceRepositoryProtocol.findOrderServiceGeneric).toHaveBeenCalledWith(query);
-    expect(result.isSuccess).toBe(false);
-    expect(result.error).toBeInstanceOf(NotOSFoundError);
-  });
+    expect(
+      orderServiceRepositoryProtocol.findOrderServiceGeneric
+    ).toHaveBeenCalledWith(query)
+    expect(result.isSuccess).toBe(false)
+    expect(result.error).toBeInstanceOf(NotOSFoundError)
+  })
 
   test('should set default values for "take" and "skip" if not provided in query', async () => {
-    orderServiceRepositoryProtocol.findOrderServiceGeneric.mockResolvedValue([]);
+    orderServiceRepositoryProtocol.findOrderServiceGeneric.mockResolvedValue([])
 
-    const query = { model: 'abc' };
-    await findOrderServiceUseCase.execute(query);
+    const query = { model: 'abc' }
+    await findOrderServiceUseCase.execute(query)
 
-    expect(orderServiceRepositoryProtocol.findOrderServiceGeneric).toHaveBeenCalledWith({
+    expect(
+      orderServiceRepositoryProtocol.findOrderServiceGeneric
+    ).toHaveBeenCalledWith({
       type: undefined,
       unit: undefined,
       date: undefined,
@@ -84,6 +98,6 @@ describe('FindOrderService', () => {
       status: undefined,
       take: 0,
       skip: 0
-    });
-  });
-});
+    })
+  })
+})
